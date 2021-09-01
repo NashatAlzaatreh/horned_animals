24cee64547bea4a0db5ed5a2db5dc6ef3166bf01
NashatAlzaatreh <nashat.alzaatra@yahoo.com>
2021-09-01 17:12:47 +0300

    making form filter

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import dataJason from "./assets/data.json";
 import SelectedBeast from "./components/SelectedBeast ";
+import Forme from "./components/Form";
 
 class App extends React.Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class App extends React.Component {
       data: dataJason,
       show: false,
       selectedData: {},
+      selectedHorne: [],
     };
   }
 
@@ -27,12 +29,34 @@ class App extends React.Component {
     });
   };
 
-  
+  // selectedHorne = dataJason.filter((value) => {
+  //   return value.horns === chosenHorn;
+  // });
+
+  // updateHornedBeast = (newHornedBeast) => {
+  //   this.setState({
+  //     dataJason: newHornedBeast,
+  //   });
+  // };
+
+  filterHorn = (event) => {
+    let numHorns = parseInt(event.target.value);
+    console.log(numHorns);
+
+    let selectedHorne = dataJason.filter((value) => {
+      return value.horns === numHorns;
+    });
+    // console.log(selectedHorne);
+    this.setState({
+      data: selectedHorne,
+    });
+  };
 
   render() {
     return (
       <div>
         <Header />
+        <Forme filterHorn={this.filterHorn} />
         <Main dataJason={this.state.data} handleShow={this.handleShow} />
         <SelectedBeast
           show={this.state.show}
--- src/components/SelectedBeast .js
+++ src/components/SelectedBeast .js
@@ -1,7 +1,7 @@
-import React from "react";
-import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 class SelectedBeast extends React.Component {
   render() {
